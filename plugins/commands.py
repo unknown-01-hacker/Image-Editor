7c70a7f4a0e967f9f3bd02547430d1158a5dd753
Abir Hasan <hackitabirhasan@gmail.com>
2021-02-03 23:07:05 +0600

    Added Force Subscribe | Now Add Channel ID sir

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,11 +1,48 @@
-# By @TroJanzHEX
+# @TroJanzHEX (Best Kanger) 😐🙄😒
+# (c) @AbirHasan2005
+
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram import Client, filters
 from script import script  # pylint:disable=import-error
+from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, UsernameNotOccupied, ChatAdminRequired, PeerIdInvalid
 
+UPDATES_CHANNEL =  # Put Channel ID
 
 @Client.on_message(filters.command(["start"]) & filters.private)
 async def start(client: Client, message: Message):
+    ## Force Sub ##
+    update_channel = UPDATES_CHANNEL
+    if update_channel:
+        try:
+            user = await client.get_chat_member(update_channel, message.chat.id)
+            if user.status == "kicked":
+               await message.reply_text(
+                   text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/linux_repo).",
+                   parse_mode="markdown",
+                   disable_web_page_preview=True
+               )
+               return
+        except UserNotParticipant:
+            await message.reply_text(
+                text="**Please Join My Updates Channel to use this Bot!**",
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton("Join Updates Channel", url=f"https://t.me/{update_channel}")
+                        ]
+                    ]
+                ),
+                parse_mode="markdown"
+            )
+            return
+        except Exception:
+            await message.reply_text(
+                text="Something went Wrong. Contact my [Support Group](https://t.me/linux_repo).",
+                parse_mode="markdown",
+                disable_web_page_preview=True
+            )
+            return
+    ## Force Sub ##
     try:
         await message.reply_text(
             text=script.START_MSG.format(message.from_user.mention),
@@ -32,6 +69,39 @@ async def start(client: Client, message: Message):
 
 @Client.on_message(filters.command(["help"]) & filters.private)
 async def help(client, message):
+    ## Force Sub ##
+    update_channel = UPDATES_CHANNEL
+    if update_channel:
+        try:
+            user = await client.get_chat_member(update_channel, message.chat.id)
+            if user.status == "kicked":
+               await message.reply_text(
+                   text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/linux_repo).",
+                   parse_mode="markdown",
+                   disable_web_page_preview=True
+               )
+               return
+        except UserNotParticipant:
+            await message.reply_text(
+                text="**Please Join My Updates Channel to use this Bot!**",
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton("Join Updates Channel", url=f"https://t.me/{update_channel}")
+                        ]
+                    ]
+                ),
+                parse_mode="markdown"
+            )
+            return
+        except Exception:
+            await message.reply_text(
+                text="Something went Wrong. Contact my [Support Group](https://t.me/linux_repo).",
+                parse_mode="markdown",
+                disable_web_page_preview=True
+            )
+            return
+    ## Force Sub ##
     try:
         await message.reply_text(
             text=script.HELP_MSG,
@@ -58,6 +128,39 @@ async def help(client, message):
 
 @Client.on_message(filters.command(["about"]) & filters.private)
 async def about(client, message):
+    ## Force Sub ##
+    update_channel = UPDATES_CHANNEL
+    if update_channel:
+        try:
+            user = await client.get_chat_member(update_channel, message.chat.id)
+            if user.status == "kicked":
+               await message.reply_text(
+                   text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/linux_repo).",
+                   parse_mode="markdown",
+                   disable_web_page_preview=True
+               )
+               return
+        except UserNotParticipant:
+            await message.reply_text(
+                text="**Please Join My Updates Channel to use this Bot!**",
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton("Join Updates Channel", url=f"https://t.me/{update_channel}")
+                        ]
+                    ]
+                ),
+                parse_mode="markdown"
+            )
+            return
+        except Exception:
+            await message.reply_text(
+                text="Something went Wrong. Contact my [Support Group](https://t.me/linux_repo).",
+                parse_mode="markdown",
+                disable_web_page_preview=True
+            )
+            return
+    ## Force Sub ##
     try:
         await message.reply_text(
             text=script.ABOUT_MSG,
