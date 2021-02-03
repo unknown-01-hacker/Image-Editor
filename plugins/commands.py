5d4abcb7e4d1d91cdbdda2ee91a78e902f2378d2
Jigar varma <71844973+Technical-Jigar@users.noreply.github.com>
2021-02-03 22:40:15 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -6,7 +6,7 @@ from pyrogram import Client, filters
 from script import script  # pylint:disable=import-error
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, UsernameNotOccupied, ChatAdminRequired, PeerIdInvalid
 
-UPDATES_CHANNEL =  # Put Channel ID
+UPDATES_CHANNEL = -1001417953950
 
 @Client.on_message(filters.command(["start"]) & filters.private)
 async def start(client: Client, message: Message):
