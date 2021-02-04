73c4782b71d38f47b30f3e5b8bf88a1b58f7a206
Jigar varma <71844973+Technical-Jigar@users.noreply.github.com>
2021-02-04 12:52:56 +0530

    Update script.py

--- script.py
+++ script.py
@@ -4,7 +4,9 @@ class script(object):
 
 I'm a Image Editor Bot which Supports various modes
 
-For more click help....</b>"""
+For more click help.... 
+
+For more help ask in @UniversalBotsSupport</b>"""
     HELP_MSG = """Hai, Follow these Steps..
 
 <code>🌀 Send me any Image to Edit..</code>
@@ -13,12 +15,11 @@ For more click help....</b>"""
 
 <code>🌀 Your Edited Image will be Uploaded </code>
 
-
-© @TroJanzHEX"""
+For more help ask in @UniversalBotsSupport"""
     ABOUT_MSG = """⭕️<b>My Name : Image Editor</b>
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
 ⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a>
 
-⭕️<b>Source Code :</b> 👉 <a href='https://github.com/TroJanzHEX/Image-Editor'>Click Here</a>"""
+⭕️<b>Devloper :</b> 👉 <a href='https://t.me/UniversalBotsUpdate'>Click Here</a>"""
